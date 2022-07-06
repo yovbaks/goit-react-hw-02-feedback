b7b81ae21b04041b9ebb4709c8ce498099465596
Serhiy Yovbak <yovbak.s@gmail.com>
2022-07-06 13:00:41 +0300

    rozmitka1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notifications/Notifications'
+
 export const App = () => {
   return (
     <div
@@ -17,6 +19,7 @@ export const App = () => {
       <Section title="Please live feedback" />
       <FeedbackOptions />
       <Section title="Statistics" />
+      <Notification message="There is no feedback"/>
       <Statistics />
     </div>
   );
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,14 +1,9 @@
 import propTypes from 'prop-types';
-import { Title } from "./Section.styled";
-const Section = ({title}) => (
-    
-        <Title>{title}</Title>
-  
-);
+import { Title } from './Section.styled';
+const Section = ({ title }) => <>{title && <Title>{title}</Title>}</>;
 
 Section.propTypes = {
-    title:propTypes.string,
-    
-}
+  title: propTypes.string,
+};
 
 export default Section;
