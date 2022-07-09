a659ce21f7cd5d7ad7794ebf91f6303df4b9e9a7
Serhiy Yovbak <yovbak.s@gmail.com>
2022-07-09 16:01:28 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,12 @@
 import propTypes from 'prop-types';
 import { Title } from './Section.styled';
-const Section = ({ title, children }) => <>{title && <Title>{title}</Title>}
-{children}</>;
+
+const Section = ({ title, children }) => (
+  <div>
+    {title && <Title>{title}</Title>}
+    {children}
+  </div>
+);
 
 Section.propTypes = {
   title: propTypes.string,
