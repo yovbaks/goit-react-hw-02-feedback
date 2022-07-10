d5999af1f18f388aba935a7c7656357e4b77b130
Serhiy Yovbak <yovbak.s@gmail.com>
2022-07-10 21:08:13 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,57 @@
+import Section from 'components/Section/Section';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Notification from 'components/Notifications';
+import Statistics from 'components/Statistics';
+import React from 'react';
 
-import Feedback from './Feddback';
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleInkremet = event => {
+		const { name } = event.currentTarget;
 
+		this.setState(prevState => ({
+			[name]: prevState[name] + 1,
+		}));
+		
+	};
+  handleGood = () => {
+    this.setState(prevState => ({ good: prevState.good + 1 }));
+    console.log('Hello Good');
+  };
 
+  handleBad = () => {
+    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+    console.log('Hello Bad');
+  };
+  handleNeutral = () => {
+    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+    console.log('Hello Neutral');
+  };
 
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((total, item) => total + item, 0);
+      
+  };
 
+    countPositiveFeedbackPercentage = () => {
+      const { good } = this.state;
+      let positiveFeedbackPercentage = (good / this.countTotalFeedback()) * 100;
+      if (!positiveFeedbackPercentage) {
+        positiveFeedbackPercentage = 0;
+      }
+      return Math.floor(positiveFeedbackPercentage);
+    };
 
-export const App = () => {
-  return (
-    <div
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback;
+    console.log(total);
+    return (
+      <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -16,11 +60,41 @@ export const App = () => {
         alignItems: 'center',
 
         color: '#010101',
-      }}
-    >
-      {/* <Counter initialValue={10} /> */}
-      <Feedback/>
-      
-    </div>
-  );
-};
+      }}>
+        <Section title="Please live feedback">
+          <FeedbackOptions
+						options={['good', 'neutral', 'bad']}
+						onLeaveFeedback={this.handleInkremet}
+					/>
+        {/* <FeedbackOptions
+          clickBad={this.handleBad}
+          clickGood={this.handleGood}
+          clickNeutral={this.handleNeutral}
+        /> */}
+       </Section>
+        
+        <Section title="Statistics">
+          {total() !== 0 ? (
+          <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positiveFeedback={this.countPositiveFeedbackPercentage()}
+        />
+        ) : (
+           <Notification message="There is no feedback" />
+        )}
+        </Section>
+        
+        
+        
+      </div>
+    );
+  }
+}
+
+
+
+
+
